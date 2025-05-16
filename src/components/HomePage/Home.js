f7dece0a3121f8e16881f4ce4efdcb1d295eb231
Mason Reichbauer <mgreichba@gmail.com>
2025-05-16 16:44:18 -0400

    modified functionality on submit alert button to turn green when clicked, modified some database components and landing page info

--- src/components/HomePage/Home.js
+++ src/components/HomePage/Home.js
@@ -10,6 +10,7 @@ export default class Home extends React.Component {
     this.state = {
       error: null,
       alerts: [],
+      buttonState: ''
     };
   }
   //gets all the users contact alerts on mount
@@ -28,6 +29,10 @@ export default class Home extends React.Component {
   //checks for geolocation availability and gets Latitude and Longitude coordinates
   sendAlert = () => {
     if (navigator.geolocation) {
+      this.setState({ buttonState: 'greenTransition' }); // Add green effect
+      setTimeout(() => {
+        this.setState({ buttonState: '' }); // Revert back to red
+      }, 2000); // hold green for 2 seconds
       navigator.geolocation.getCurrentPosition(this.showPosition);
     } else {
       alert("Geolocation is not supported by this browser.");
@@ -51,21 +56,21 @@ export default class Home extends React.Component {
   render() {
     const alerts = this.state.alerts.map((alert, idx) => (
       <p key={idx}>
-        <span className='bold'>  User: </span>{alert.nick_name}
-        <span className='bold'>  Alert Time: </span>{moment(alert.alert_time).format("dddd, MMMM Do YYYY, h:mm:ss a")}
-        <span className='bold'>  Longitude: </span>{alert.longitude}
-        <span className='bold'>  Latitude: </span>{alert.latitude}
-        <span className='bold'>  Safeword: </span>{alert.safeword}
+        <span className='bold'>  User: </span>{alert.nick_name} <br/>
+        <span className='bold'>  Alert Time: </span>{moment(alert.alert_time).format("dddd, MMMM Do YYYY, h:mm:ss a")} <br/>
+        <span className='bold'>  Longitude: </span>{alert.longitude} <br/>
+        <span className='bold'>  Latitude: </span>{alert.latitude} <br/>
+        <span className='bold'>  Safeword: </span>{alert.safeword} <br/>
         <span className='bold'>  Emergency: </span>{alert.alert_active ? <span className='redAlert'>"Emergency"</span> : <span className='greenAlert'>"Safe"</span>}
       </p>
     ))
     return (
       <div>
         <img id="Landing-Logo" src={Logo} alt="Live Alert Logo" className="logo" />
-        <button className="alertButton" onClick={this.sendAlert}>Send Alert!</button>
+        <button className={`alertButton ${this.state.buttonState}`} onClick={this.sendAlert}>Send Alert!</button>
 
         <div className='alertsHome'>
-          <h2>Alerts Received</h2>
+          <h2>Alerts From Friends</h2>
           <div className='userAlerts'>{alerts}</div>
 
         </div>
